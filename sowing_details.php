47ad2cf4f5f5da7e1b2758a50ebc32c642f40a1e
Alexander Pukhalskyi <p.vadim974@gmail.com>
2023-07-28 09:51:35 +0300

    Edit sowing_details

--- sowing_details.php
+++ sowing_details.php
@@ -1,56 +1,62 @@
-<?
+<?php
 
-// Соединямся с БД
-$link=mysqli_connect("localhost", "root", "admin","Garden");
+$link = mysqli_connect("localhost", "root", "admin", "Garden");
 
-if (isset($_COOKIE['id']) && isset($_COOKIE['hash']))
-{
-    if (isset($_POST['add'])) {
-        header("Location: new_crop.php");
-    }elseif (isset($_POST['update'])){
-        header("Location: update_crop.php");
+if (isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
+    if (isset($_POST['back'])) {
+        header("Location: sowing_main.php");
+    } elseif (isset($_POST['home_page'])) {
+        header("Location: home_page.php");
     }
-    $id = intval($_COOKIE['id']);
-    $result_sowing = mysqli_query($link, "SELECT * FROM Sowing WHERE Id_Sowing = $id");
-    $result_area = mysqli_query($link, "SELECT * FROM Area WHERE ID_Sowing = $id");
-    $result_line = mysqli_query($link, "SELECT Seed.Name_Seed FROM Sowing
-                                              JOIN Area ON Sowing.Id_Sowing = Area.ID_Sowing
-                                              JOIN Line ON Area.Id_Area = Line.ID_Area
-                                              JOIN Seed ON Line.ID_Seed = Seed.Id_Seed
-                                              WHERE Sowing.Id_Sowing = $id");
 
-    if (mysqli_num_rows($result_sowing) > 0) {
-        // Выводим таблицу HTML
-        $row = mysqli_fetch_assoc($result_sowing);
-        echo "<table>";
-        echo "<tr><th>Назва посіву</th></tr>";
-        echo "<tr>";
-        echo "<td>" . $row['Name_Sowing'] . "</td>";
-        echo "</tr>";
-        // Проходимся по каждой записи результата
-        while ($row_area = mysqli_fetch_assoc($result_area)) {
-            $number_area=1;
+    if (isset($_GET['id'])) {
+        $id = intval($_GET['id']);
+        $result_sowing = mysqli_query($link, "SELECT * FROM Sowing WHERE Id_Sowing = $id");
+        $result_area = mysqli_query($link, "SELECT * FROM Area WHERE ID_Sowing = $id");
+
+        if (mysqli_num_rows($result_sowing) > 0) {
+            $row = mysqli_fetch_assoc($result_sowing);
             echo "<table>";
-            echo "<tr><th>Ділянка номер $number_area</th></tr>";
+            echo "<tr><th>Назва посіву</th></tr>";
             echo "<tr>";
-            echo "<td>";
-            while ($row_line = mysqli_fetch_assoc($result_line)) {
-                echo "<tr>";
-                echo "<td>" . $row_line['Name_Seed'] . "</td>";
-                echo "</tr>";
-            }
-            echo "</td>";
+            echo "<td>" . $row['Name_Sowing'] . "</td>";
             echo "</tr>";
-            echo "</table>";
-            $number_area++;
-        }
 
-        echo "</table>";
+            $number_area = 1;
+            while ($row_area = mysqli_fetch_assoc($result_area)) {
+                $area_id = $row_area['Id_Area'];
+                echo "<tr><th>Ділянка номер $number_area</th></tr>";
+                echo "<tr>";
+                echo "<td>";
+                $result_line = mysqli_query($link, "SELECT Seed.Name_Seed FROM Sowing
+                                                  JOIN Area ON Sowing.Id_Sowing = Area.ID_Sowing
+                                                  JOIN Line ON Area.Id_Area = Line.ID_Area
+                                                  JOIN Seed ON Line.ID_Seed = Seed.Id_Seed
+                                                  WHERE Area.Id_Area = $area_id AND Sowing.Id_Sowing = $id");
+
+                while ($row_line = mysqli_fetch_assoc($result_line)) {
+                    echo "<tr>";
+                    echo "<td>" . $row_line['Name_Seed'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</td>";
+                echo "</tr>";
+                $number_area++;
+            }
+
+            echo "</table>";
+        } else {
+            echo "Посів не знайдено";
+        }
     } else {
-        echo "Посів не знайдено";
+        echo "Не передано ID посіву.";
     }
-} else
-{
+} else {
     print "Включите куки";
 }
 ?>
+
+<form method="POST">
+    <input name="back" type="submit" value="Назад">
+    <input name="home_page" type="submit" value="На головну сторінку">
+</form>
--- sowing_main.php
+++ sowing_main.php
@@ -1,24 +1,21 @@
-<?
+<?php
 
-// Соединямся с БД
-$link=mysqli_connect("localhost", "root", "admin","Garden");
+$link = mysqli_connect("localhost", "root", "admin", "Garden");
 
-if (isset($_COOKIE['id']) && isset($_COOKIE['hash']))
-{
-  if (isset($_POST['add'])) {
-      header("Location: new_crop.php");
-  }elseif (isset($_POST['update'])){
-      header("Location: update_crop.php");
-  }
+if (isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
+    if (isset($_POST['add'])) {
+        header("Location: new_crop.php");
+    } elseif (isset($_POST['update'])) {
+        header("Location: update_crop.php");
+    }
+
     $id = intval($_COOKIE['id']);
     $result = mysqli_query($link, "SELECT * FROM Sowing WHERE ID_Acount = $id");
 
     if (mysqli_num_rows($result) > 0) {
-        // Выводим таблицу HTML
         echo "<table>";
         echo "<tr><th>Назва посіву</th><th>Деталі</th></tr>";
 
-        // Проходимся по каждой записи результата
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
             echo "<td>" . $row['Name_Sowing'] . "</td>";
@@ -30,8 +27,7 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['hash']))
     } else {
         echo "У вас немає посівів.";
     }
-} else
-{
+} else {
     print "Включите куки";
 }
 ?>
