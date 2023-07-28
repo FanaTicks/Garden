f6a56bf24010c8ed78320b83841f74b879f0f6a9
Alexander Pukhalskyi <p.vadim974@gmail.com>
2023-07-28 09:32:41 +0300

    Edit sowing_details

--- sowing_details.php
+++ sowing_details.php
@@ -13,7 +13,11 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['hash']))
     $id = intval($_COOKIE['id']);
     $result_sowing = mysqli_query($link, "SELECT * FROM Sowing WHERE Id_Sowing = $id");
     $result_area = mysqli_query($link, "SELECT * FROM Area WHERE ID_Sowing = $id");
-    $result_line = mysqli_query($link, "SELECT * FROM Line WHERE Id_Sowing = $id");
+    $result_line = mysqli_query($link, "SELECT Seed.Name_Seed FROM Sowing
+                                              JOIN Area ON Sowing.Id_Sowing = Area.ID_Sowing
+                                              JOIN Line ON Area.Id_Area = Line.ID_Area
+                                              JOIN Seed ON Line.ID_Seed = Seed.Id_Seed
+                                              WHERE Sowing.Id_Sowing = $id");
 
     if (mysqli_num_rows($result_sowing) > 0) {
         // Выводим таблицу HTML
@@ -25,15 +29,20 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['hash']))
         echo "</tr>";
         // Проходимся по каждой записи результата
         while ($row_area = mysqli_fetch_assoc($result_area)) {
+            $number_area=1;
             echo "<table>";
+            echo "<tr><th>Ділянка номер $number_area</th></tr>";
             echo "<tr>";
             echo "<td>";
             while ($row_line = mysqli_fetch_assoc($result_line)) {
-
+                echo "<tr>";
+                echo "<td>" . $row_line['Name_Seed'] . "</td>";
+                echo "</tr>";
             }
             echo "</td>";
             echo "</tr>";
             echo "</table>";
+            $number_area++;
         }
 
         echo "</table>";
